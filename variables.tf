b1eae5fe67ec413f49ec91b4f318e7c698c095d7
FANCY TUBA <158304347+Fancytuba06@users.noreply.github.com>
2024-03-18 21:33:14 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,80 @@
+variable "A_Name" {
+   type = string
+   description = "A"
+}
+
+variable "B_cidr_block" {
+   type = string
+   description = "B"
+}
+
+variable "C_Name" {
+   type = string
+   description = "C"
+}
+
+variable "D_cidr_block" {
+   type = string
+   description = "D"
+}
+
+variable "E_Name" {
+   type = string
+   description = "E"
+}
+
+variable "F_cidr_block" {
+   type = string
+   description = "F"
+}
+
+variable "G_Name" {
+   type = string
+   description = "G"
+}
+
+variable "H_Name" {
+   type = string
+   description = "H"
+}
+
+variable "I_cidr_block" {
+   type = string
+   description = "I"
+}
+
+variable "J_ami" {
+   type = string
+   description = "J"
+}
+
+variable "K_instance_type" {
+   type = string
+   description = "K"
+}
+
+variable "L_key_name" {
+   type = string
+   description = "L"
+}
+
+variable "M_cidr_blocks" {
+   type = list(string)
+   description = "M"
+}
+
+variable "N_cidr_blocks" {
+   type = list(string)
+   description = "N"
+}
+
+variable "O_cidr_blocks" {
+   type = list(string)
+   description = "O"
+}
+
+variable "P_Name" {
+   type = string
+   description = "p"
+}
+
